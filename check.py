5b4561fad39abe5331454c1adc8fe631f1947b22
Canyon Read <canyonread@gmail.com>
2020-02-19 00:12:52 -0800

    Add files via upload
    Removed unnecessary variables and made variable for opened file more understandable.

--- check.py
+++ check.py
@@ -1,15 +1,13 @@
 def status(location):
 # Gets status from the cur.txt associated with location
 # (passed through from main.py)
-	with open(f'{location}_cur.txt','r') as cur:
-	    mStat = cur.read(5)
-	return mStat
+    with open(f'{location}_cur.txt','r') as txt:
+        return txt.readline()
 
 
 def timestamp(location):
 # Gets timestamp from the cur.txt associated with location
 # (passed through from main.py)
-	with open(f'{location}_cur.txt','r') as cur:
-	    _x = cur.readline()
-	    mTime = cur.readline()
-	return mTime
+    with open(f'{location}_cur.txt','r') as txt:
+        _x = txt.readline()
+        return txt.readline()

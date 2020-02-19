02465b8c5bc1e8b8d0aaaf7894e06982b79b26b2
Canyon Read <canyonread@gmail.com>
2020-02-19 12:50:33 -0800

    Add files via upload

--- check.py
+++ check.py
@@ -1,13 +1,13 @@
 def status(location):
 # Gets status from the cur.txt associated with location
-# (passed through from main.py)
+# (passed through from casa.py)
     with open(f'{location}_cur.txt','r') as txt:
         return txt.readline()
 
 
 def timestamp(location):
 # Gets timestamp from the cur.txt associated with location
-# (passed through from main.py)
+# (passed through from casa.py)
     with open(f'{location}_cur.txt','r') as txt:
-        _x = txt.readline()
+        txt.readline(0)
         return txt.readline()
